031ae6401e1004ac09b055be130c474a652bdefe
Ivanov-666 <114649215+Ivanov-666@users.noreply.github.com>
2024-11-27 10:29:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from elevator_system import ElevatorSystem
 
-requests = [[1, 10], [3, 11], [2, 6], [9, 4], [8, 9], [11, 5], [7, 4], [6, 3], [10, 11], [12, 8], [5, 2], [1, 7], [4, 12], [6, 8], [3, 5]]
 elevator_system = ElevatorSystem(12, [12, 4])
-elevator_system.run(requests)
+
+elevator_system.run(1, 10)
 elevator_system.get_history()
